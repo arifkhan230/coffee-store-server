fbf5ad37784dc0ebfc47171f6862543210d2ead0
arifkhan101 <103602241+arifkhan101@users.noreply.github.com>
2023-10-15 15:12:48 +0600

    ready to deploy

--- index.js
+++ index.js
@@ -19,12 +19,21 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middleware 
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req,res)=>{
+    res.send('Coffee making server is running')
+})
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     const coffeeCollection = client.db('coffeeDB').collection('coffee')
+    const userCollection = client.db('coffeeDB').collection('user')
 
 
     app.get('/coffee', async(req,res)=>{
@@ -75,6 +84,40 @@ async function run() {
         res.send(result)
     })
 
+    // user related api
+
+    app.get('/user',async(req,res)=>{
+      const cursor = userCollection.find();
+      const users = await cursor.toArray();
+      res.send(users)
+    })
+
+    app.post('/user', async(req,res)=>{
+        const user =req.body;
+        console.log(user)
+        const result = await userCollection.insertOne(user)
+        res.send(result)
+    })
+
+    app.patch('/user', async(req,res)=>{
+      const user = req.body;
+      const filter = {email: user.email}
+      const updateDoc = {
+        $set:{
+          lastLoggedAt: user.lastLoggedAt 
+        }
+      }
+      const result =await userCollection.updateOne(filter,updateDoc)
+      res.send(result)
+    })
+
+    app.delete('/user/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
 
     // Send a ping to confirm a successful connection
@@ -88,13 +131,7 @@ async function run() {
 run().catch(console.dir);
 
 
-// middleware 
-app.use(cors())
-app.use(express.json())
 
-app.get('/', (req,res)=>{
-    res.send('Coffee making server is running')
-})
 
 app.listen(port,()=>{
     console.log(`coffee server is is running on port: ${port}`)
